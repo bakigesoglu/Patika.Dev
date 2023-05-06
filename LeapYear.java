fd44e45de8225fd52402e072fdb4142d719c1eab
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-05-06 22:20:13 +0300

    LeapYear

--- LeapYear.java
+++ LeapYear.java
@@ -8,21 +8,9 @@ public class LeapYear {
         int year;
         System.out.print("Enter the year to find the leap year: ");
         year = inp.nextInt();
-        int a = year % 100;
-        System.out.println(a);
-        if (year % 100 == 0) {
-            if (year % 16 == 0) { // 400 e bölünme şartı 16 ve 25 e bölünme olduğu için. 400= 2^4*5^2
-                                  //100 e  bölünen sayı 25 e bölündüğü için 25 için şart oluşturmadık.
+
+        if (year % 100 != 0 &&  year % 4==0 || year%400==0) { //Artık yılı bulmak için koşullarımızı olusturduk.
                 System.out.print(year + " is leap year");
-            }
-            else {
-                System.out.print(year + " is not leap year.");
-            }
-        }
-        else {
-            if (year%4==0)
-            {
-                System.out.print(year + " is leap year.");
             }
             else {
                 System.out.print(year + " is not leap year.");
@@ -30,4 +18,3 @@ public class LeapYear {
         }
 
     }
-}

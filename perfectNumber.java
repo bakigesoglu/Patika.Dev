71b51373c473b966a5855b83b44008be397a3c64
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-05-13 19:10:17 +0300

    perfectNumber

---
+++ perfectNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class perfectNumber {
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number=inp.nextInt();
+        int  total=0;
+        for(int i=1;i<number;i++){
+            if(number%i==0){
+                total=total+i;
+            }
+        }if(total==number){
+            System.out.println(number+" is a perfect number");
+        }else {
+            System.out.println(number+" is not a perfect number.");
+        }
+    }
+}

5c91130f1b6c2a528658b08d7565f7074cd138e0
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-05-13 20:32:51 +0300

    invertedTriangle

--- invertedTriangle.java
+++ invertedTriangle.java
@@ -4,15 +4,13 @@ public class invertedTriangle {
         Scanner inp = new Scanner(System.in);
         System.out.print("Enter the how many steps will the inverted triangle have  : ");
         int n = inp.nextInt();
-
-        for (int i = 0; i<n; i++) {
-
-            for (int k = n; k>=n-i; k--) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j <n- i; j++) {
                 System.out.print(" ");
-            }
-            for (int j=2*n-3; j >=2*i-1;j--) {
+                }
+            for (int k = 0; k < 2 * i - 1; k++) {
                 System.out.print("*");
-            }
+                }
             System.out.println();
         }
     }

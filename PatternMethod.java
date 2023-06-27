15c616ed04b08b9d39ba7ac31eb731fc157b37fd
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-06-28 00:12:29 +0300

    PatternMethod

--- PatternMethod.java
+++ PatternMethod.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class PatternMethod {
-    static void pattern(int number, int k) {
+    static void pattern(int number, int k) { //Azalan desene göre metodumuzu oluşturduk.
         if (number > 0) {
             System.out.print(number + " ");
             number -= 5;
             pattern(number, k);
-        } else {
+        } else {                            // number 0 ve altına düşünce 2. metodumuzu cagırıyor.
             pattern2(number, k);
         }
     }
 
-    static void pattern2(int number, int k) {
+    static void pattern2(int number, int k) { // Artış metodu
         if (number <= k) {
             System.out.print(number + " ");
             number += 5;

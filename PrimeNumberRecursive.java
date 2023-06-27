92a423bbcef0e7ba41bb1f107a0b3cd8853f6ba9
Baki Gesoğlu <bakigesoglu1@gmail.com>
2023-06-27 22:20:10 +0300

    Update PrimeNumberRecursive.java

--- PrimeNumberRecursive.java
+++ PrimeNumberRecursive.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class PrimeNumberRecursive {
     static boolean isPrime(int number,int n) {
-        if (n ==  1) {
+        if (n ==  1) {   // 1 e kadar n 1 er azalacak ve hala kalan sıfır olan yoksa n=1 durumunda sayı asal olur.
             return true;
         } else {
-            if (number % n == 0) {
+            if (number % n == 0) { // eğer n den 1 er geriye giderken n değeri ile bölümden kalan 0 ise asal degildir.
                 return false;
             } else {
                 return isPrime(number,n-1);
@@ -16,10 +16,10 @@ public class PrimeNumberRecursive {
     public static void main (String [] Args)
     {
 
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        Scanner inp = new Scanner(System.in); 
+        System.out.print("Enter a number: ");  // Kullanıcıdan sayı girişi alıyoruz.
         int number = inp.nextInt();
-        boolean result = isPrime(number,number/2);
+        boolean result = isPrime(number,number/2); //girilen sayı ve girilen sayının yarısı ile sorgulama yapmamız asal sayıyı aramaya yeterli.
 
         if (result) {
             System.out.print(number + " is a prime number.");
